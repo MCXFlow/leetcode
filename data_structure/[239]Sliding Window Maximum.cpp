88547e1a6b4716c705405da5caec4b37361fa39b
MCXFlow <mcxflow@outlook.com>
2023-07-31 13:47:41 +0800

    holiday - 2023/07/31

---
+++ data_structure/[239]Sliding Window Maximum.cpp
@@ -0,0 +1,69 @@
+//You are given an array of integers nums, there is a sliding window of size k 
+//which is moving from the very left of the array to the very right. You can only 
+//see the k numbers in the window. Each time the sliding window moves right by one 
+//position. 
+//
+// Return the max sliding window. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+//Output: [3,3,5,5,6,7]
+//Explanation: 
+//Window position                Max
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1], k = 1
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 1 <= k <= nums.length 
+// 
+//
+// Related Topics Array Queue Sliding Window Heap (Priority Queue) Monotonic 
+//Queue 👍 15279 👎 504
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<pair<int, int>> heap;
+        for (int i = 0; i < k; ++i) {
+            heap.emplace(nums[i], i);
+        }
+        vector<int> ans{heap.top().first};
+        for (int i = 1; i <= n - k; ++i) {
+            heap.emplace(nums[i+k-1], i+k-1);
+            while (heap.top().second < i) {
+                heap.pop();
+            }
+            ans.push_back(heap.top().first);
+        }
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
